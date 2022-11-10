965df5b00262086f7a7ce6a91ed511b2a12a83c7
Lari Hotari <lari@hotari.net>
2022-11-10 15:08:13 +0200

    Make the app a simple Spring Boot app
    responds to
    curl -X POST -d '{"name":"mytopic"}' -H 'Content-Type: application/json' http://localhost:8080/topics

--- app/src/main/java/org/apache/pulsar/experiment/app/App.java
+++ app/src/main/java/org/apache/pulsar/experiment/app/App.java
@@ -1,7 +1,28 @@
 package org.apache.pulsar.experiment.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 
+    public record TopicName(String name) {
+
+    }
+
+    @RestController
+    public static class Controller {
+
+        @PostMapping("/topics")
+        public Mono<String> createTopic(@RequestBody TopicName topicName) {
+            return Mono.just("Creating " + topicName);
+        }
     }
 }
