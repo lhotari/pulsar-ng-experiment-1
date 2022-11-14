318755e2e52666e0497eb0bc5ee96d8cd5b3af4b
Lari Hotari <lari@hotari.net>
2022-11-14 20:43:09 +0200

    Extract constants

--- app/src/main/java/org/apache/pulsar/experiment/Constants.java
+++ app/src/main/java/org/apache/pulsar/experiment/Constants.java
@@ -10,13 +10,16 @@ import org.apache.ratis.protocol.RaftPeer;
 import org.apache.ratis.thirdparty.com.google.protobuf.ByteString;
 
 public class Constants {
+    public static final int PEER_COUNT = 3;
+    public static final int STARTING_PORT = 10050;
+    public static final RaftGroupId RAFT_GROUP_ID = RaftGroupId.valueOf(ByteString.copyFromUtf8("helloworld123456"));
     public static List<RaftPeer> RAFT_PEERS = createPeers();
 
     private static List<RaftPeer> createPeers() {
         List<RaftPeer> peers = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < PEER_COUNT; i++) {
             peers.add(RaftPeer.newBuilder()
-                    .setAddress(InetSocketAddress.createUnresolved("localhost", 10050 + i))
+                    .setAddress(InetSocketAddress.createUnresolved("localhost", STARTING_PORT + i))
                     .setId("peer" + i)
                     .build());
         }
@@ -26,6 +29,6 @@ public class Constants {
     public static RaftGroup RAFT_GROUP = createRaftGroup();
 
     private static RaftGroup createRaftGroup() {
-        return RaftGroup.valueOf(RaftGroupId.valueOf(ByteString.copyFromUtf8("helloworld123456")), RAFT_PEERS);
+        return RaftGroup.valueOf(RAFT_GROUP_ID, RAFT_PEERS);
     }
 }
