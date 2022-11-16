afcc531c3a7cf6b29c7e8f21990bacc6def4c5fb
Lari Hotari <lari@hotari.net>
2022-11-16 10:22:05 +0200

    Enable Ratis metrics

--- app/src/main/java/org/apache/pulsar/experiment/shard/app/MetadataServer.java
+++ app/src/main/java/org/apache/pulsar/experiment/shard/app/MetadataServer.java
@@ -4,18 +4,22 @@ import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 import org.apache.ratis.conf.RaftProperties;
 import org.apache.ratis.grpc.GrpcConfigKeys;
+import org.apache.ratis.metrics.JVMMetrics;
 import org.apache.ratis.protocol.RaftGroup;
 import org.apache.ratis.protocol.RaftPeer;
 import org.apache.ratis.server.RaftServer;
 import org.apache.ratis.server.RaftServerConfigKeys;
 import org.apache.ratis.util.NetUtils;
+import org.apache.ratis.util.TimeDuration;
 
 public final class MetadataServer implements Closeable {
     private final RaftServer server;
 
     public MetadataServer(RaftGroup raftGroup, RaftPeer peer, File storageDir) throws IOException {
+        JVMMetrics.initJvmMetrics(TimeDuration.valueOf(60, TimeUnit.SECONDS));
         final RaftProperties properties = new RaftProperties();
         RaftServerConfigKeys.setStorageDir(properties, Collections.singletonList(storageDir));
         final int port = NetUtils.createSocketAddr(peer.getAddress()).getPort();
